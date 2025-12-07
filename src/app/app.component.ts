cd7edd684edf606554e50bac103a8476dded5de7
borsosbalazselek <borsosbalazs1979@gmail.com>
2025-12-07 18:29:25 +0100

    angular base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'autorobottech';
+
+  onSubmit(e: Event) {
+  e.preventDefault();
+  alert('Köszönjük! Hamarosan jelentkezünk.');
+}
 }
